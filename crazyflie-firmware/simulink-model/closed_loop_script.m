d385fc2e6a5c583119c720db52305c723746b912
JohanEddeland <JEDDELAN@volvocars.com>
2017-04-27 11:00:55 +0200

    Updated script for Assignment 5

--- crazyflie-firmware/simulink-model/closed_loop_script.m
+++ crazyflie-firmware/simulink-model/closed_loop_script.m
@@ -1,7 +1,13 @@
 %% Breach script for closed_loop.slx
 
+%% Set inport switch
+% We need to set the inport switch so that it reads the "From Breach"
+% inports
+load_system('closed_loop');
+set_param('closed_loop/Manual Switch','sw','1');
+
 %% Initialize Breach
-% Disable some warnings that makes the script look ugly
+% Disable some warnings that make the script look ugly
 warning('off', 'Simulink:Engine:OutputNotConnected');
 warning('off', 'Simulink:Engine:InputNotConnected');
 
@@ -16,22 +22,33 @@ inputs = B.Sys.InputList;
 disp('Inputs:');
 disp(inputs);
 
+
+
 %% Create input generators
 % We create input generators for all inputs of the system
 % As we saw in the 'inputs' list, the input of the system are:
-% - Roll_reference
-% - Pitch_reference
-% - Yaw_reference
-% - Yawrate_reference
+% - Base_Thrust
+% - Ref_Roll
+% - Ref_Pitch
+% - Ref_YawRate
 
-% You have 4 different input generators at your disposal:
-% - pulse_signal_gen generates pulse signals. You can set the base value,
-%   period, pulse width, amplitude and delay
+% We can generate signals just by having fixed control points. 
+
+% NOT MANDATORY:
+% You can also try out any of these 3 other input generators:
+% - pulse_signal_gen
+% - var_cp_signal_gen
+% - var_step_signal_gen 
+% To use them, you can just comment out the fixed_cp_signal_gen and instead
+%   uncomment the generator you want to use. 
+
+% NOTE: It's up to you how many control points to use - you don't
+%   necessarily have to use 3. 
 
 % Pitch reference input generator: choose one by commenting/uncommenting!
-thrust_gen    = pulse_signal_gen({'Base_Thrust'});
-% thrust_gen    = fixed_cp_signal_gen({'Base_Thrust'},... % Signal name
-%                                     3); % Number of control points
+% thrust_gen    = pulse_signal_gen({'Base_Thrust'});
+thrust_gen    = fixed_cp_signal_gen({'Base_Thrust'},... % Signal name
+                                    3); % Number of control points
 % thrust_gen    = var_cp_signal_gen({'Base_Thrust'},... % Signal name
 %                                     3); % Number of control points
 % thrust_gen    = var_step_signal_gen({'Base_Thrust'},... % Signal name
@@ -48,21 +65,21 @@ roll_gen    = fixed_cp_signal_gen({'Ref_Roll'}, ... % Signal name
 
 % Pitch reference input generator: choose one by commenting/uncommenting!
 % pitch_gen    = pulse_signal_gen({'Ref_Pitch'});
-% pitch_gen    = fixed_cp_signal_gen({'Ref_Pitch'},... % Signal name
-%                                     3); % Number of control points
-pitch_gen    = var_cp_signal_gen({'Ref_Pitch'},... % Signal name
+pitch_gen    = fixed_cp_signal_gen({'Ref_Pitch'},... % Signal name
                                     3); % Number of control points
+% pitch_gen    = var_cp_signal_gen({'Ref_Pitch'},... % Signal name
+%                                     3); % Number of control points
 % pitch_gen    = var_step_signal_gen({'Pitch_reference'},... % Signal name
 %                                     3); % Number of control points
 
 % Pitch reference input generator: choose one by commenting/uncommenting!
 % yawrate_gen    = pulse_signal_gen({'Ref_YawRate'});
-% yawrate_gen    = fixed_cp_signal_gen({'Ref_YawRate'},... % Signal name
-%                                     3); % Number of control points
+yawrate_gen    = fixed_cp_signal_gen({'Ref_YawRate'},... % Signal name
+                                    3); % Number of control points
 % yawrate_gen    = var_cp_signal_gen({'Ref_YawRate'},... % Signal name
 %                                     3); % Number of control points
-yawrate_gen    = var_step_signal_gen({'Ref_YawRate'},... % Signal name
-                                    3); % Number of control points
+% yawrate_gen    = var_step_signal_gen({'Ref_YawRate'},... % Signal name
+%                                     3); % Number of control points
 
 
 
@@ -77,21 +94,24 @@ B.SetInputGen(InputGen);
 % generators
 B.PrintParams();
 
+% Note that all the parameters have been initiated to 0. 
+% NOTE: To get interesting simulations, you should change them all!
+
 %% Set input generator parameters
 
-% The input parameters get standard values assigned to them
-% We can change them manually in the following way:
-B.SetParam({'Base_Thrust_base_value','Base_Thrust_pulse_amp','Base_Thrust_pulse_period'},...
-                        [1 20000 4]);
+% We can change the parameters manually in the following way:
+B.SetParam({'Base_Thrust_u0','Base_Thrust_u1','Base_Thrust_u2'},...
+                        [20000 30000 14000]);
 B.SetParam({'Ref_Roll_u0', 'Ref_Roll_u1', 'Ref_Roll_u2'},...
-                        [1 0 2]);
-B.SetParam({'Ref_Pitch_dt0', 'Ref_Pitch_dt1'},...
-                        [2 5]);               
+                        [5 0 10]);          
                     
 % For some input parameters we might want to assign a RANGE of values. 
 % This is done in the following way:
-%B.SetParamRanges({'Ref_Pitch_u0', 'Ref_Pitch_u1', 'Ref_Pitch_u2'},...
-%                        [-1 1; -1 1; -1 1]);
+B.SetParamRanges({'Ref_Pitch_u0', 'Ref_Pitch_u1', 'Ref_Pitch_u2'},...
+                       [-10 10; -10 10; -10 10]);
+                   
+% Print the parameters again to see the changed values
+B.PrintParams();
                     
 %% Sample parameters
 % For all parameters that have a range defined, we can sample parameter
@@ -105,22 +125,26 @@ B.Sim(); % Simulate the system
 
 %% Plot results
 % We can plot each signal against its reference for the first scenario ...
+%% Figure 1
 B.PlotSignals({'Base_Thrust'},...% Signals to plot
 	[1],... % Which scenario to plot (only the first)
 	{},... % Additional options
 	1); % Boolean indicating that signals should be plotted in ONE figure 
 
-B.PlotSignals({'Roll_reference','Roll'},... % Signals to plot
+%% Figure 2
+B.PlotSignals({'Ref_Roll','Roll'},... % Signals to plot
 	[1],... % Which scenario to plot (only the first)
 	{},... % Additional options
 	1); % Boolean indicating that signals should be plotted in ONE figure
 
-B.PlotSignals({'Pitch_reference','Pitch'},...% Signals to plot
+%% Figure 3
+B.PlotSignals({'Ref_Pitch','Pitch'},...% Signals to plot
 	[1],... % Which scenario to plot (only the first)
 	{},... % Additional options
 	1); % Boolean indicating that signals should be plotted in ONE figure
 
-B.PlotSignals({'Yaw_reference','Yaw'},...% Signals to plot
+%% Figure 4
+B.PlotSignals({'Ref_YawRate','Yaw'},...% Signals to plot
 	[1],... % Which scenario to plot (only the first)
 	{},... % Additional options
 	1); % Boolean indicating that signals should be plotted in ONE figure      
@@ -128,7 +152,10 @@ B.PlotSignals({'Yaw_reference','Yaw'},...% Signals to plot
 
             
 % ... and we can also plot ALL signals in ALL scenarios in ONE figure
+%% Figure 5
 B.PlotSignals({'Roll_reference','Roll','Pitch_reference','Pitch','Yaw_reference','Yaw'});
+
+%% Figure 6
 B.PlotSignals({'Base_Thrust','x','y','z'});
             
 
@@ -147,38 +174,40 @@ B.PrintSignals();
 % - Yaw_reference[t]
 % - Yawrate_reference[t]
 
-% We want to see if the yaw is always close to its reference or not
+% We want to see if the pitch is always close to its reference or not
 % The STL specification for this could be:
-yaw_close_to_ref = STL_Formula('yaw_close_to_ref','alw_[0,TotalSimulationTime](abs(Yawrate[t] - Ref_YawRate[t]) < tol)');
+pitch_close_to_ref = STL_Formula('pitch_close_to_ref','alw_[0,TotalSimulationTime](abs(Pitch[t] - Ref_Pitch[t]) < tol)');
 % What should the tolerance be? Let's try with 0.1
-yaw_close_to_ref = set_params(yaw_close_to_ref,{'tol'},[0.1]);
+pitch_close_to_ref = set_params(pitch_close_to_ref,{'tol'},[0.1]);
+
+% HINT: This is where you can add other STL formulas to try to falsify!
 
 % Check the specification against the simulations we performed
-yaw_results = B.CheckSpec(yaw_close_to_ref);
+pitch_results = B.CheckSpec(pitch_close_to_ref);
 disp('Spec satisfaction results:')
-disp(yaw_results);
+disp(pitch_results);
 
 % Negative results mean that the robustness is negative, i.e., the
 %   specification is not fulfilled. 
 % We can plot the robustness function of the specification for a more
 %   detailed view.
-B.PlotRobustSat(yaw_close_to_ref,... % Specification to plot
+B.PlotRobustSat(pitch_close_to_ref,... % Specification to plot
     inf,... % Depth of formula to plot
     [],... % Time instants where to evaluate
     1);
 
-% Ok, so the tolerance of 0.1 was probably too tight ... how about 0.8?
-yaw_close_to_ref2 = set_params(yaw_close_to_ref,{'tol'},[0.8]);
+% Ok, so the tolerance of 0.1 was probably too tight ... how about 15?
+pitch_close_to_ref2 = set_params(pitch_close_to_ref,{'tol'},[15]);
 
 % Check specification again
-yaw_results2 = B.CheckSpec(yaw_close_to_ref2);
+pitch_results2 = B.CheckSpec(pitch_close_to_ref2);
 disp('Spec satisfaction results, second try:')
-disp(yaw_results2);
+disp(pitch_results2);
 
 % Positive results mean that the specification is fulfilled for all three
 %   scenarios!
 % To verify the results, we can plot robustness for scenario 1 again:
-B.PlotRobustSat(yaw_close_to_ref2,... % Specification to plot
+B.PlotRobustSat(pitch_close_to_ref2,... % Specification to plot
     inf,... % Depth of formula to plot
     [],... % Time instants where to evaluate
     1);
@@ -189,17 +218,17 @@ B.PlotRobustSat(yaw_close_to_ref2,... % Specification to plot
 % To tackle this question, we use FALSIFICATION. 
 
 % First, we create a Breach falsification problem
-falsif_pb = FalsificationProblem(B, yaw_close_to_ref2);
+falsif_pb = FalsificationProblem(B, pitch_close_to_ref2);
 % Now, we let Breach attempt to solve it!
 res = falsif_pb.solve();
-                    
+
 if ~isempty(res)
     % Breach managed to falsify the specification
     B_False = falsif_pb.GetBrSet_False();
     % B_False is the same as B, but containing the falsified trajectory
     
     % Plot the robustness to visualize why the specification was not held
-    B_False.PlotRobustSat(yaw_close_to_ref2);
+    B_False.PlotRobustSat(pitch_close_to_ref2);
 end
 
 
@@ -209,7 +238,7 @@ end
 % To answer this, we use parameter synthesis
 
 % Create a parameter synthesis problem
-synth_pb = ParamSynthProblem(B, yaw_close_to_ref2, {'tol'},  [0 1]);
+synth_pb = ParamSynthProblem(B, pitch_close_to_ref2, {'tol'},  [0 1]);
 
 % Let Breach solve it
 synth_pb.solve();
